ae830565a56e63994a29df5b9630772f30762c9a
Jesús Antonio Ceme Yam <131736251+JesusACY04@users.noreply.github.com>
2023-11-26 17:19:26 -0500

    Add files via upload

---
+++ Reloj/Reloj_Java/src/main/java/com/mycompany/reloj_java/NewClassReloj.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.reloj_java;
+import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class NewClassReloj extends Thread {
+    
+    public void run(){
+        while(true){
+            try {
+                Calendar clr=Calendar.getInstance();
+                int hr=clr.get(Calendar.HOUR);
+                int min=clr.get(Calendar.MINUTE);
+                int sg=clr.get(Calendar.SECOND);
+                
+                String tiempo= "Hora= "+ hr+ ": "+ min+ ": "+ sg;
+                
+                System.out.println(tiempo);
+                
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(NewClassReloj.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
